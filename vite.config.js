6a7191d0e5cbafa2c754e3e8fe1313861e7ed529
Raj Shakya <rs7267887611@gmail.com>
2024-12-13 12:06:56 +0530

    Trying to resolve 404 error

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/revana/', // Make sure this matches your GitHub repository name
+  plugins: [
+    react(),
+    createHtmlPlugin(),
+  ],
+  base: '/revana/', // Ensure this matches your GitHub repository name
 });
